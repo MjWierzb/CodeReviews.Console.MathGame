659697775c8850d9fe782d6f65e8e29bd77b35b6
Maciej "MJ" Wierzbinski <166155193+MjWierzb@users.noreply.github.com>
2025-03-04 09:09:56 -0500

    Update Program.cs
    Removed unused local variable

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -6,9 +6,6 @@ var menu = new Menu();
 /* define variable for today's date*/
 var date = DateTime.UtcNow;
 
-/*define variable to store history of games played*/
-var games = new List<string>();
-
 /* define variable for user's name input by assigning it GetName method*/
 string name = Helpers.GetName();
 
